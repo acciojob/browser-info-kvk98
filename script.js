b8bc9e3c32992e8a2e9668610f04a4a7281da8c2
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-22 13:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 let div = document.createElement("div");
 div.id = "browser-info";
+body.append(div);
 
 let browserName = navigator.appName;
 div.innerText = "You are using " + browserName + "version " + navigator.appVersion;
