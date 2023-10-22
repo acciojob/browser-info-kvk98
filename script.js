63bb577f4e5fd6bc343703a74730365ebcb7fb8b
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-22 13:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 let div = document.createElement("div");
 div.id = "browser-info";
+
+let body = getElementsByTagName("body").[0];
 body.append(div);
 
 let browserName = navigator.appName;
