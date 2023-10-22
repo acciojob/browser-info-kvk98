229794061c2b8349100c5c1c3009a100c5707b84
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-22 16:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// let div = document.createElement("div");
+// div.id = "browser-info";
+
+// let body = getElementsByTagName("body")[0];
+// body.append(div);
+
+// let browserName = navigator.appName;
+// let text = "You are using " + browserName + "version " + navigator.appVersion;
+// function textContent() {
+// 	div.innerHTML = text;
+// }
+
+// body.onload = textContent;
+
 let div = document.createElement("div");
 div.id = "browser-info";
 
-let body = getElementsByTagName("body")[0];
+let body = document.getElementsByTagName("body")[0]; // Corrected getElementsByTagName
 body.append(div);
 
 let browserName = navigator.appName;
-let text = "You are using " + browserName + "version " + navigator.appVersion;
-function textContent() {
-	div.innerHTML = text;
+let textContent = "You are using " + browserName + " version " + navigator.appVersion; // Use a different variable name to store the text
+
+function setText() {
+  div.innerHTML = textContent; // Set the content of the div
 }
 
-body.onload = textContent;
+body.onload = setText; // Assign the setText function to the body's onload event
